5a73f888703dfbb7f49458e497b596193ac33d28
nicolewolff <81988673+nicolewolff@users.noreply.github.com>
2023-07-20 13:48:52 -0700

    Update inject_spaced_arg.py

--- inject_spaced_arg.py
+++ inject_spaced_arg.py
@@ -19,50 +19,51 @@ decay_rate = 0
 dm_profile = 'nfw'
 
 
-""" Injects a Gaussian signal at a specified center frequency
-:param xs: frequencies
-:type xs: 1darray
-:param ys: powers
-:type ys: 1darray
-:param theta: angle between target and Sun's velocity vector [deg]
-:type theta: float
-:param sig_loc: center frequency
-:type sig_loc: float
-:param sig_std: Doppler broadening of the source
-:type sig_std: float
-:param sig_amp: weight given to a signal's amplitude depending on DM density profile and annihilation/decay formula
-:type sig_amp: float
-:return: power spectrum with an added Gaussian signal every 50 MHz 
-:rtype: 1darray
-"""
 def inject(xs, ys, theta, sig_loc, sig_std, sig_amp):
+    """ Injects a Gaussian signal at a specified center frequency
+    :param xs: frequencies
+    :type xs: 1darray
+    :param ys: powers
+    :type ys: 1darray
+    :param theta: angle between target and Sun's velocity vector [deg]
+    :type theta: float
+    :param sig_loc: center frequency
+    :type sig_loc: float
+    :param sig_std: Doppler broadening of the source
+    :type sig_std: float
+    :param sig_amp: weight given to a signal's amplitude depending on DM density profile and annihilation/decay formula
+    :type sig_amp: float
+    :return: power spectrum with an added Gaussian signal every 50 MHz 
+    :rtype: 1darray
+    """
     shift_factor = get_shift_factor(theta)
     sig_loc, sig_std, sig_amp = sig_loc*shift_factor, sig_std*shift_factor, sig_amp/shift_factor
     return (ys + sig_amp*np.exp(-(xs - sig_loc)**2 / (2*sig_std**2)))  # Gaussian signal
 
-""" Calculates Doppler shift
-:param theta: Angle between target and Sun's velocity vector [deg]
-:type theta: float
-:return: Scalar Doppler factor for frequencies
-:rtype: float
-"""
-def get_shift_factor(theta):    
+
+def get_shift_factor(theta):
+    """ Calculates Doppler shift
+    :param theta: Angle between target and Sun's velocity vector [deg]
+    :type theta: float
+    :return: Scalar Doppler factor for frequencies
+    :rtype: float
+    """
     beta = v_earth / c
     radians_from_v_earth = np.radians(theta)
     return 1 + beta*np.cos(radians_from_v_earth)
 
-""" Injects a series of spaced signals into a spectrum 
-:param ann_cross_sec: annihilation cross section, an analog for signal strength
-:type ann_cross_sec: float
-:param loc: starting frequency for injection loop
-:type loc: float
-:param filepath_to_save: Directory path to save injected spectra
-:type filepath_to_save: string
-:param template: Whether or not to inject a signal onto a template (an array of 1s)
-:type template: Boolean
-:return: None
-"""
 def inject_spaced(ann_cross_sec, loc, filepath_to_save, template):
+    """ Injects a series of spaced signals into a spectrum 
+    :param ann_cross_sec: annihilation cross section, an analog for signal strength
+    :type ann_cross_sec: float
+    :param loc: starting frequency for injection loop
+    :type loc: float
+    :param filepath_to_save: Directory path to save injected spectra
+    :type filepath_to_save: string
+    :param template: Whether or not to inject a signal onto a template (an array of 1s)
+    :type template: Boolean
+    :return: None
+    """
     os.makedirs(filepath_to_save, exist_ok=True)
     if is_decay:
         sig_std = v_virial*sig_loc/(c*np.sqrt(3))

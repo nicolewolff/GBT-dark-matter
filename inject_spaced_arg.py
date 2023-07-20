17b684086b33b4516dd4285ce47e366b1b5a1a14
nicolewolff <81988673+nicolewolff@users.noreply.github.com>
2023-07-20 11:49:06 -0700

    Update inject_spaced_arg.py
    Finalized docstring comments

--- inject_spaced_arg.py
+++ inject_spaced_arg.py
@@ -13,33 +13,55 @@ template = False
 
 v_virial = 250
 v_earth = 225
-sig_loc = 1390
+sig_loc = 1390  # approx midpoint of the L Band
 c = 299792
 decay_rate = 0
 dm_profile = 'nfw'
 
 
-"""
-:param sig_loc: Center frequency ued to calculate signal magnitude
+""" Injects a Gaussian signal at a specified center frequency
+:param xs: frequencies
+:type xs: 1darray
+:param ys: powers
+:type ys: 1darray
+:param theta: angle between target and Sun's velocity vector [deg]
+:type theta: float
+:param sig_loc: center frequency
 :type sig_loc: float
 :param sig_std: Doppler broadening of the source
 :type sig_std: float
-:param sig_amp: Weight given to a signal's amplitude depending on DM density profile and annihilation/decay formula
+:param sig_amp: weight given to a signal's amplitude depending on DM density profile and annihilation/decay formula
 :type sig_amp: float
 :return: power spectrum with an added Gaussian signal every 50 MHz 
-:rtype: float
+:rtype: 1darray
 """
 def inject(xs, ys, theta, sig_loc, sig_std, sig_amp):
     shift_factor = get_shift_factor(theta)
     sig_loc, sig_std, sig_amp = sig_loc*shift_factor, sig_std*shift_factor, sig_amp/shift_factor
     return (ys + sig_amp*np.exp(-(xs - sig_loc)**2 / (2*sig_std**2)))  # Gaussian signal
-    
-def get_shift_factor(theta):
-    
+
+""" Calculates Doppler shift
+:param theta: Angle between target and Sun's velocity vector [deg]
+:type theta: float
+:return: Scalar Doppler factor for frequencies
+:rtype: float
+"""
+def get_shift_factor(theta):    
     beta = v_earth / c
     radians_from_v_earth = np.radians(theta)
     return 1 + beta*np.cos(radians_from_v_earth)
 
+""" Injects a series of spaced signals into a spectrum 
+:param ann_cross_sec: annihilation cross section, an analog for signal strength
+:type ann_cross_sec: float
+:param loc: starting frequency for injection loop
+:type loc: float
+:param filepath_to_save: Directory path to save injected spectra
+:type filepath_to_save: string
+:param template: Whether or not to inject a signal onto a template (an array of 1s)
+:type template: Boolean
+:return: None
+"""
 def inject_spaced(ann_cross_sec, loc, filepath_to_save, template):
     os.makedirs(filepath_to_save, exist_ok=True)
     if is_decay:
@@ -50,7 +72,7 @@ def inject_spaced(ann_cross_sec, loc, filepath_to_save, template):
         sig_std = v_virial*sig_loc/(c*np.sqrt(6))
         sig_amp_ratio = 3.55e28*ann_cross_sec / (v_virial*(sig_loc/1e3)**4) # convert MHz to GHz
         col_name ='nfw_sq'    
-    targ_info = pd.read_csv('/home/dataadmin/GBTData/SharedDataDirectory/lband/data_table/all_info.csv').set_index('file_name')
+    targ_info = pd.read_csv(data_table_path).set_index('file_name')
     names = os.listdir(inject_path)
     for name in tqdm(names):
         try:

817196c1f9572f3d93e396a64711781419a10a2b
nicolewolff <81988673+nicolewolff@users.noreply.github.com>
2023-07-20 11:05:50 -0700

    Create pval_runner.py
    Compatibility with config file

---
+++ pval_runner.py
@@ -0,0 +1,19 @@
+from pval_finder2 import main_function
+import os
+import sys 
+import configparser
+from tqdm import tqdm
+
+def main(start_frequencies, signal_sizes, config): 
+    os.makedirs("PValRunnerLogs", exist_ok=True)
+
+    for freq in start_frequencies:
+        for signal in signal_sizes:
+            main_function(freq, signal, config)
+
+if __name__ == '__main__': 
+    config = configparser.ConfigParser()
+    config.read(sys.argv[1])
+    start_frequencies = [int(arg) for arg in config.get("Settings", "start_frequencies").split(",")]
+    signal_sizes = [float(arg) for arg in config.get("Settings", "signal_sizes").split(",")]
+    main(start_frequencies, signal_sizes, config)

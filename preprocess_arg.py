e72f2f11febf01c350d97a12f71fd32d54d59b42
nicolewolff <81988673+nicolewolff@users.noreply.github.com>
2023-08-09 17:25:45 -0700

    Update preprocess_arg.py

--- preprocess_arg.py
+++ preprocess_arg.py
@@ -6,9 +6,6 @@ import numpy as np
 
 sys.path.insert(0, '..')
 
-#what we want to preprocess
-#file_path_to_extract = '/home/dataadmin/GBTData/SharedDataDirectory/sband/injected_limits_analysis/start_1800/signal_1e-43'
-
 def preprocess(file_path_to_extract, file_path_to_save, template): 
     os.makedirs(file_path_to_save, exist_ok=True)
     files = os.listdir(file_path_to_extract); 
@@ -20,7 +17,6 @@ def preprocess(file_path_to_extract, file_path_to_save, template):
         'worker_id': -1, 
         'template': False, 
     } 
-    # import pdb; 
     for i in range(0, len(files), num_workers): 
         for j in range(1, num_workers+1): 
             if i + j >= len(files): 
